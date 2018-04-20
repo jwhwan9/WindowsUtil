9c73b61b9ac4a84d5b5bb2d0bb88dc1658165a85
jwhwan9 <jwhwang@gmail.com>
2018-04-20 09:47:24 +0800

    Add files via upload
    Query DNS Records include MSFT ActiveDirectory Records
    :\>QDns.exe [Domain Name] [TCP | UDP] [DC | KDC | GC]

---
+++ qDNS/Program.cs
@@ -0,0 +1,170 @@
+﻿/**********************************************************************
+ * Copyright (c) 2010, j. montgomery                                  *
+ * All rights reserved.                                               *
+ *                                                                    *
+ * Redistribution and use in source and binary forms, with or without *
+ * modification, are permitted provided that the following conditions *
+ * are met:                                                           *
+ *                                                                    *
+ * + Redistributions of source code must retain the above copyright   *
+ *   notice, this list of conditions and the following disclaimer.    *
+ *                                                                    *
+ * + Redistributions in binary form must reproduce the above copyright*
+ *   notice, this list of conditions and the following disclaimer     *
+ *   in the documentation and/or other materials provided with the    *
+ *   distribution.                                                    *
+ *                                                                    *
+ * + Neither the name of j. montgomery's employer nor the names of    *
+ *   its contributors may be used to endorse or promote products      *
+ *   derived from this software without specific prior written        *
+ *   permission.                                                      *
+ *                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS*
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT  *
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS  *
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE     *
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,*
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES           *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,*
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)      *
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED*
+ * OF THE POSSIBILITY OF SUCH DAMAGE.                                 *
+ **********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using DnDns.Enums;
+using DnDns.Query;
+using DnDns.Records;
+using DnDns.Security;
+
+namespace qDnDns
+{
+    class Program
+    {
+        static string Protocol = "UDP";
+        static string Domain = "google.com";
+        static string AD_Prefix = "";
+
+        static string DC_Prefix = "_ldap._tcp.dc._msdcs.";
+        static string KDC_Prefix = "_kerberos._tcp.dc._msdcs.";
+        static string GC_Prefix = "_ldap._tcp.gc._msdcs.";
+
+        static void Main(string[] args)
+        {
+            DnsQueryRequest request = new DnsQueryRequest();
+            DnsQueryResponse response = null;
+
+
+            Console.WriteLine("QDns [Domain Name] [TCP | UDP] [DC | KDC | GC]");
+
+            if (args.Length == 1)
+            {
+                Domain = args[0];
+            }
+            else if (args.Length == 2)
+            {
+                Domain = args[0];
+                Protocol = args[1];
+            }
+            else if (args.Length == 3)
+            {
+                Domain = args[0];
+                Protocol = args[1];
+
+                if (args[2].ToUpper().Equals("DC"))
+                {
+                    AD_Prefix = DC_Prefix;
+                }
+                else if (args[2].ToUpper().Equals("KDC"))
+                {
+                    AD_Prefix = KDC_Prefix;
+                }
+                else if (args[2].ToUpper().Equals("GC"))
+                {
+                    AD_Prefix = GC_Prefix;
+                }
+            }
+
+            Domain = AD_Prefix + Domain;
+
+            if (Protocol.ToUpper().Equals("TCP"))
+            {
+                // Test TCP request            
+                response = request.Resolve(Domain, NsType.A, NsClass.INET, ProtocolType.Tcp);
+
+            }
+            else if (Protocol.ToUpper().Equals("UDP"))
+            {
+                // Test UDP request            
+                //response = request.Resolve(Domain, NsType.A, NsClass.INET, ProtocolType.Udp);
+                //response = request.Resolve(Domain, NsType., NsClass.INET, ProtocolType.Udp);
+                response = request.Resolve(Domain, NsType.ANY, NsClass.ANY, ProtocolType.Udp);
+            }
+
+            if (response != null)
+            {
+                OutputResults(response);
+            }
+
+            // Example usage of security provider.
+            // DnsQueryRequest request3 = new DnsQueryRequest();
+            // DnsQueryResponse response3 = request3.Resolve("a.tsig.secured.server", "a.server.to.lookup", NsType.A, NsClass.INET, ProtocolType.Udp, new TsigMessageSecurityProvider("sharedkeyname", "shared key as base 64 string", 300));
+
+        }
+
+        private static void OutputResults(DnsQueryResponse response)
+        {
+            Console.WriteLine("Bytes received: " + response.BytesReceived);
+
+            Console.WriteLine("Name: " + response.Name);
+            Console.WriteLine("OpCode: " + response.NsClass);
+            Console.WriteLine("NsFlags: " + response.NsFlags);
+            Console.WriteLine("NsType: " + response.NsType);
+            Console.WriteLine("RCode: " + response.RCode);
+            Console.WriteLine("OpCode: " + response.OpCode);
+
+            // Enumerate the Answer Records
+            Console.WriteLine("Answers:");
+            foreach (IDnsRecord record in response.Answers)
+            {
+                Console.WriteLine(record.Answer + " " + record.DnsHeader.Name);
+                Console.WriteLine("  |--- RDATA Field Length: " + record.DnsHeader.DataLength);
+                Console.WriteLine("  |--- Name: " + record.DnsHeader.Name);
+                Console.WriteLine("  |--- NS Class: " + record.DnsHeader.NsClass);
+                Console.WriteLine("  |--- NS Type: " + record.DnsHeader.NsType);
+                Console.WriteLine("  |--- TTL: " + record.DnsHeader.TimeToLive);
+                Console.WriteLine();
+            }
+
+            foreach (IDnsRecord record in response.AuthoritiveNameServers)
+            {
+                Console.WriteLine(record.Answer + " " + record.DnsHeader.Name);
+                Console.WriteLine("  |--- RDATA Field Length: " + record.DnsHeader.DataLength);
+                Console.WriteLine("  |--- Name: " + record.DnsHeader.Name);
+                Console.WriteLine("  |--- NS Class: " + record.DnsHeader.NsClass);
+                Console.WriteLine("  |--- NS Type: " + record.DnsHeader.NsType);
+                Console.WriteLine("  |--- TTL: " + record.DnsHeader.TimeToLive);
+                Console.WriteLine();
+            }
+
+            foreach (IDnsRecord record in response.AdditionalRRecords)
+            {
+                Console.WriteLine(record.Answer + " " + record.DnsHeader.Name);
+                Console.WriteLine("  |--- RDATA Field Length: " + record.DnsHeader.DataLength);
+                Console.WriteLine("  |--- Name: " + record.DnsHeader.Name);
+                Console.WriteLine("  |--- NS Class: " + record.DnsHeader.NsClass);
+                Console.WriteLine("  |--- NS Type: " + record.DnsHeader.NsType);
+                Console.WriteLine("  |--- TTL: " + record.DnsHeader.TimeToLive);
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
