d14fbf87d981e0a56ebab4440f8ec6d3d1a12ca7
jwhwan9 <jwhwang@gmail.com>
2018-11-16 17:56:09 +0800

    Add files via upload

--- QWinSystemEvent/QWinSystemEvent/Program.cs
+++ QWinSystemEvent/QWinSystemEvent/Program.cs
@@ -18,7 +18,7 @@ namespace QWinSystemEvent
 
         enum IREvent
         {
-            DateTime, MachineName, RecordID, LogName, EventID, Task, TaskID, Provider, Brief
+            DateTime, MachineName, RecordID, Level, LogName, EventID, Task, TaskID, Provider, Brief, Misc
         };
         #endregion
 
@@ -32,12 +32,14 @@ namespace QWinSystemEvent
             IREventHeader[(int)IREvent.DateTime] = "DateTime";
             IREventHeader[(int)IREvent.MachineName] = "MachineName";
             IREventHeader[(int)IREvent.RecordID] = "RecordID";
-            IREventHeader[(int)IREvent.LogName] = "LogName";
+            IREventHeader[(int)IREvent.Level] = "Level";
+            IREventHeader[(int)IREvent.LogName] = "LogName";            
             IREventHeader[(int)IREvent.EventID] = "EventID";
             IREventHeader[(int)IREvent.Task] = "Task";
             IREventHeader[(int)IREvent.TaskID] = "TaskID";
             IREventHeader[(int)IREvent.Provider] = "Provider";
             IREventHeader[(int)IREvent.Brief] = "Brief";
+            IREventHeader[(int)IREvent.Misc] = "Misc";
             #endregion
 
             if (isConsoleOutput)
@@ -85,7 +87,8 @@ namespace QWinSystemEvent
             filteredEventID = new System.Collections.Generic.Dictionary<int, int>();
 
 
-            // Enriched Event                         
+            // Enriched Event               
+            
             filteredEventID.Add(12, 12); // PC Power on.           
             filteredEventID.Add(13, 13); // PC Power off.           
 
@@ -93,8 +96,25 @@ namespace QWinSystemEvent
             filteredEventID.Add(19, 19); // Windows Update Installed
             filteredEventID.Add(27, 27); // Windows Update Paused    
 
-            filteredEventID.Add(45058, 45058); // 登入快取項目是最舊的項目而且已經移除
+            filteredEventID.Add(37, 37); // Time Service
 
+            filteredEventID.Add(41, 41); // Kernel Power
+
+            filteredEventID.Add(1502, 1502); // Group Policy OK
+            
+            filteredEventID.Add(1030, 1030); // Group Policy Failed
+            
+            filteredEventID.Add(6005, 6005); // EventLog
+            filteredEventID.Add(6006, 6006); // EventLog Stop
+            filteredEventID.Add(6008, 6008); // EventLog 意外關機
+            filteredEventID.Add(6013, 6013); // EventLog System Start Time
+
+            filteredEventID.Add(7000, 7000); // System Error
+            //filteredEventID.Add(7036, 7036); // Service Status
+            filteredEventID.Add(7023, 7023); // System Error
+
+            filteredEventID.Add(45058, 45058); // 登入快取項目是最舊的項目而且已經移除
+            
             filteredEventCondition = new System.Collections.Generic.Dictionary<string, string>();
 
             using (var reader = new EventLogReader(EventLogFile, PathType.FilePath))
@@ -106,71 +126,117 @@ namespace QWinSystemEvent
                 int i = 0;
                 while ((record = reader.ReadEvent()) != null)
                 {
-
-                    using (record)
+                    #region begin parse event
+                    try
                     {
-                        if (!filteredEventID.ContainsKey(record.Id)) continue;
 
-                        var meta = new ProviderMetadata(record.ProviderName).Events.Where(evt => evt.Id == record.Id).FirstOrDefault();
-                        IList<EventProperty> Properties = record.Properties;
-
-                        if (!isFilterOut(record))
+                        using (record)
                         {
+                            if (!filteredEventID.ContainsKey(record.Id)) continue;
 
-                            string[] IREventLog = new string[Enum.GetNames(typeof(IREvent)).Length];
+                            var meta = new ProviderMetadata(record.ProviderName).Events.Where(evt => evt.Id == record.Id).FirstOrDefault();
+                            IList<EventProperty> Properties = record.Properties;
 
-                            #region Common Event Info
-                            IREventLog[(int)IREvent.DateTime] = record.TimeCreated.Value.ToString("u");
-                            IREventLog[(int)IREvent.MachineName] = record.MachineName;
-                            IREventLog[(int)IREvent.RecordID] = record.RecordId.ToString();
-                            IREventLog[(int)IREvent.LogName] = record.LogName;
-                            IREventLog[(int)IREvent.EventID] = record.Id.ToString();
-                            IREventLog[(int)IREvent.Task] = record.TaskDisplayName;
-                            IREventLog[(int)IREvent.TaskID] = record.Task.ToString();
-                            IREventLog[(int)IREvent.Provider] = record.ProviderName.ToString();
-                            try
-                            {
-                                IREventLog[(int)IREvent.Brief] = record.FormatDescription().Split(new[] { '\r', '\n' }).FirstOrDefault();
-                            }
-                            catch (Exception ex)
-                            {
-                                IREventLog[(int)IREvent.Brief] = record.FormatDescription();
-                            }
-                            #endregion
-
-                            #region 12，電腦開機
-                            if (record.Id == 12)
+                            if (!isFilterOut(record))
                             {
 
-                                #region Subject Info
-                                IREventLog[(int)IREvent.Task] = "電腦開機";
-                                
+                                string[] IREventLog = new string[Enum.GetNames(typeof(IREvent)).Length];
+
+                                #region Common Event Info
+                                IREventLog[(int)IREvent.DateTime] = record.TimeCreated.Value.ToString("u");
+                                IREventLog[(int)IREvent.MachineName] = record.MachineName;
+                                IREventLog[(int)IREvent.RecordID] = record.RecordId.ToString();
+                                IREventLog[(int)IREvent.LogName] = record.LogName;
+                                IREventLog[(int)IREvent.Level] = record.LevelDisplayName;
+                                IREventLog[(int)IREvent.EventID] = record.Id.ToString();
+                                IREventLog[(int)IREvent.Task] = record.TaskDisplayName;
+                                IREventLog[(int)IREvent.TaskID] = record.Task.ToString();
+                                IREventLog[(int)IREvent.Provider] = record.ProviderName.ToString();
+
+                                try
+                                {
+                                    IREventLog[(int)IREvent.Brief] = record.FormatDescription().Replace(System.Environment.NewLine, "<br>");//record.FormatDescription().Split(new[] { '\r', '\n' }).FirstOrDefault();
+                                }
+                                catch (Exception ex)
+                                {
+                                    IREventLog[(int)IREvent.Brief] = record.FormatDescription();
+                                }
                                 #endregion
 
-                            }
-                            #endregion
+                                #region 12，電腦開機
+                                if (record.Id == 12)
+                                {
 
-                            #region 13，電腦關機
-                            else if (record.Id == 13)
-                            {
+                                    #region Subject Info
+                                    IREventLog[(int)IREvent.Task] = "電腦開機";
 
-                                #region Subject Info
-                                IREventLog[(int)IREvent.Task] = "電腦關機";
+                                    #endregion
 
+                                }
                                 #endregion
 
-                            }
-                            #endregion
+                                #region 13，電腦關機
+                                else if (record.Id == 13)
+                                {
 
-                            dumpStrings2CSV(IREventLog);
-                            #region Enable Dump Detailed Event Log
-                            //dumpEventRecordSchema(record);
-                            #endregion
+                                    #region Subject Info
+                                    IREventLog[(int)IREvent.Task] = "電腦關機";
+
+                                    #endregion
+
+                                }
+                                #endregion
+                                #region 6013，Uptime
+                                else if (record.Id == 6013)
+                                {
+
+                                    #region Subject Info
+                                    IREventLog[(int)IREvent.Task] = "Uptime";
+
+                                    #endregion
+
+                                }
+                                #endregion
+                                #region 1502，GPO.OK
+                                else if (record.Id == 1502)
+                                {
+
+                                    #region Subject Info
+                                    IREventLog[(int)IREvent.Task] = "GPO.OK";
+
+                                    #endregion
+
+                                }
+                                #endregion
+                                #region 1030，GPO.Failed
+                                else if (record.Id == 1030)
+                                {
+
+                                    #region Subject Info
+                                    IREventLog[(int)IREvent.Task] = "GPO.Failed";
+
+                                    #endregion
+
+                                }
+                                #endregion
+
+                                dumpStrings2CSV(IREventLog);
+                                #region Enable Dump Detailed Event Log
+                                //dumpEventRecordSchema(record);
+                                #endregion
+                            }
+
+
                         }
 
+                    }
+                    catch (Exception ex)
+                    {
 
                     }
+                    #endregion
                 }
+                    
             }
         }
 
@@ -193,7 +259,10 @@ namespace QWinSystemEvent
 
         static void dumpEventRecordSchema(EventRecord eventRecord)
         {
+            if (eventRecord.ProviderName == null) return;
             var meta = new ProviderMetadata(eventRecord.ProviderName).Events.Where(evt => evt.Id == eventRecord.Id).FirstOrDefault();
+
+            if (meta == null) return;
             string myMeta = meta.Description;
 
             IList<EventProperty> Properties = eventRecord.Properties;
